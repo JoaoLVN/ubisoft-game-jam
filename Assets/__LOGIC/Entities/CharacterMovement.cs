90d68cce78b5b7c043e1ce5829e6f9fff98d0bf7
patrickalfa <patrick.c.alfa@gmail.com>
2020-11-28 12:07:22 +0100

    Split PlayerController into two separate scripts.

---
+++ Assets/__LOGIC/Entities/CharacterMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class CharacterMovement : MonoBehaviour
+{
+    public bool Drop {
+        get { return _drop; }
+        set { _drop = value; }
+    }
+    public bool Use {
+        get { return _use; }
+        set { _use = value; }
+    }
+    public Vector2 Forward { get { return _forward; } }
+    public Vector2 Right { get { return Quaternion.Euler(0, 0, 90) * _forward; } }
+    [SerializeField] private float _moveSpeed = 5f;
+    private Vector2 _forward = Vector2.zero;
+    private Rigidbody2D _rigidbody;
+
+    private Vector2 _moveDirection = Vector2.zero;
+    private bool _drop;
+    private bool _use;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponentInChildren<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+        Debug.DrawRay(transform.position, Forward);
+    }
+
+    private void HandleMovement()
+    {
+        _rigidbody.velocity = _moveDirection * _moveSpeed;
+    }
+
+    public void Move(Vector2 direction)
+    {
+        _moveDirection = direction;
+
+        if (direction.magnitude != 0)
+            _forward = direction;
+    }
+}
--- Assets/__LOGIC/Entities/Player/PlayerController.cs
+++ Assets/__LOGIC/Entities/Player/PlayerController.cs
@@ -1,21 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+[RequireComponent(typeof(CharacterMovement))]
 public class PlayerController : MonoBehaviour
 {
-    public bool Drop { get { return _drop; } }
-    public bool Use { get { return _use; } }
-    public Vector2 Forward { get { return _forward; } }
-    public Vector2 Right { get { return Quaternion.Euler(0, 0, 90) * _forward; } }
-    [SerializeField] private float _moveSpeed = 5f;
-    private Vector2 _forward = Vector2.zero;
-    private Vector2 _moveDirection = Vector2.zero;
-    private Rigidbody2D _rigidbody;
-    private bool _drop;
-    private bool _use;
-    private void Awake()
+    private CharacterMovement _characterMovement;
+    private Vector2 movementAxis;
+
+    private void Start()
     {
-        _rigidbody = GetComponentInChildren<Rigidbody2D>();
+        _characterMovement = GetComponent<CharacterMovement>();
     }
 
     private void Update()
@@ -23,23 +18,14 @@ public class PlayerController : MonoBehaviour
         ProcessInputs();
     }
 
-    private void FixedUpdate()
-    {
-        Move();
-        Debug.DrawRay(transform.position, Forward);
-    }
-
     private void ProcessInputs()
     {
-        _moveDirection.x = Input.GetAxis("Horizontal");
-        _moveDirection.y = Input.GetAxis("Vertical");
-        _moveDirection.Normalize();
-        if (_moveDirection.magnitude != 0) _forward = new Vector2(_moveDirection.x, _moveDirection.y);
-        _drop = Input.GetButton("Drop");
-        _use = Input.GetButton("Use");
-    }
-    private void Move()
-    {
-        _rigidbody.velocity = _moveDirection * _moveSpeed;
+        movementAxis.x = Input.GetAxis("Horizontal");
+        movementAxis.y = Input.GetAxis("Vertical");
+        movementAxis.Normalize();
+
+        _characterMovement.Move(movementAxis);
+        _characterMovement.Drop = Input.GetButton("Drop");
+        _characterMovement.Use = Input.GetButton("Use");
     }
 }
