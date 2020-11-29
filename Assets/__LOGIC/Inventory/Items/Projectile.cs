a5c352b71bce85cdf765f2e1e6a0320381c8525f
João Luiz Vieira <joao.euu@gmail.com>
2020-11-29 11:21:07 +0100

    Implement ranged weapons.

---
+++ Assets/__LOGIC/Inventory/Items/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour
+{
+    private Character _character;
+    private int _damage;
+
+    public void Setup(Character character, int damage, Vector3 direction, float force)
+    {
+        _character = character;
+        _damage = damage;
+        GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var character = other.gameObject.GetComponent<Character>();
+        if (character != null && character != _character)
+        {
+            character.ApplyDamage(_damage);
+        }
+        if (character != _character)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/__LOGIC/Inventory/Items/RangedWeapon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RangedWeapon : Item
+{
+    [SerializeField] private Projectile _projectile;
+    [SerializeField] private float _force = 10;
+    [SerializeField] private int _damage = 1;
+    [SerializeField] private float _cooldown = 2;
+    [SerializeField] private float _attackDuration = .25f;
+    private bool _ready = true;
+    public override void Use()
+    {
+        if (!_ready) return;
+
+        Animator[] animators = _controller.GetComponentsInChildren<Animator>();
+        foreach (Animator animator in animators)
+            animator.SetTrigger("Attack");
+        Character character = _controller.GetComponent<Character>();
+        character.Stun(_attackDuration);
+
+        SoundManager.PlaySound("woosh", false, .5f);
+
+        StartCoroutine(CooldownRoutine());
+
+        GameObject projectile = GameObject.Instantiate(_projectile.gameObject, _controller.Pivot.position, Quaternion.identity);
+        projectile.SetActive(true);
+        projectile.GetComponent<Projectile>().Setup(character, _damage, _controller.Forward, _force);
+
+    }
+
+
+    private IEnumerator CooldownRoutine()
+    {
+        _ready = false;
+        yield return new WaitForSeconds(_cooldown);
+        while (_controller != null && _controller.Use) yield return null;
+        _ready = true;
+    }
+
+
+}
