e40d7702965b6f85d1081588d63f498622659a83
João Luiz Vieira <joao.euu@gmail.com>
2020-11-29 10:35:33 +0100

    Remove debug log.

--- Assets/CheckoutItemAnimationState.cs
+++ Assets/CheckoutItemAnimationState.cs
@@ -4,7 +4,6 @@ public class CheckoutItemAnimationState : StateMachineBehaviour
 {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log( Mathf.Max(animator.GetInteger("CheckoutItems") - 1, 0));
         animator.SetInteger("CheckoutItems", Mathf.Max(animator.GetInteger("CheckoutItems") - 1, 0));
     }
 }
